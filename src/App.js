ed65459c68c8b18dcca41f92a971f5e33cae5848
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-14 13:16:24 +0300

    Create React App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Header from './header/Header.jsx';
+import Main from './main/Main.jsx';
+import Footer from './footer/Footer.jsx';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+
+export default App;
