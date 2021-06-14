6e2d900bb31f24ae73255178a8617ac8b7e7ba48
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-14 23:51:38 +0300

    Added main markup

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,9 +1,90 @@
 import React from 'react';
+import blockImg1 from './images/block_image_1.jpg';
+import blockImg2 from './images/block_image_2.jpg';
 
 
 const Main = () => {
   return (
-    'main'
+    <>
+      <section className="slider">
+        <div className="static-container">
+          <div className="trend">
+            Оснащение конференц-залов
+          </div>
+
+          <h1>
+            Видео конференции
+          </h1>
+
+          <div className="description">
+            Профессиональное оборудование для конференц-залов
+            предназначено для проведения и организации презентаций,
+            совещаний, бизнес-тренингов и онлайн-трансляции контента.
+          </div>
+
+          <button className="back-call">
+            Обратный звонок
+          </button>
+
+          <button className="fill-out-brief">
+            Заполнить бриф
+          </button>
+        </div>
+
+        <div className="dynamic-container">
+          <div className="slide first active">
+            <div className="number">
+              01
+            </div>
+
+            <div className="description">
+              Простота использования оснащения<br />
+              позволяет решать функциональные и<br />
+              организационные задачи,<br />
+              оптимизировать работу, проводить<br />
+              мероприятия различного масштаба.
+            </div>
+          </div>
+
+          <div className="vertical-line" />
+
+          <div className="slide second inactive">
+            <div className="number">
+              02
+            </div>
+
+            <div className="description">
+              Применение инновационных решений для<br />
+              демонстрации культурных и исторических<br />
+              реликвий – актуальная тенденция, которая<br />
+              успешно применяется в музеях и на<br />
+              площадках альтернативного искусства.
+            </div>
+          </div>
+        </div>
+
+        <div className="arrows-container">
+          <button className="prev-slide" />
+          <button className="next-slide" />          
+        </div>
+      </section>
+
+      <section className="block">
+        <div className="img-container">
+          <img src={blockImg1} alt="block-img" />
+        </div>
+
+        <div className="img-container">
+          <img src={blockImg2} alt="block-img" />
+        </div>
+
+        <div className="pop-up">
+          <button>
+            +
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
