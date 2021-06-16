186b13a546dca2d14b69fcbd9ada5b3315c62896
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-16 17:28:37 +0300

    Added PopUpCall state and context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import './App.scss';
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Header from './header/Header.jsx';
 import Main from './main/Main.jsx';
+import PopUpCall from './PopUpCall.jsx';
 import Footer from './footer/Footer.jsx';
 
 
+export const PopUpCallContext = createContext();
+
+
 function App() {
+  const [ popUpCallOpacity, setPopUpCallOpacity ] = useState(0);
+
+  const togglePopUpCall = () => {
+    setPopUpCallOpacity(popUpCallOpacity === 1 ? 0 : 1);
+  }
+
   return (
-    <>
+    <PopUpCallContext.Provider
+      value={{
+        popUpCallOpacity,
+        togglePopUpCall
+      }}
+    >
       <Header />
       <Main />
+      <PopUpCall />
       <Footer />
-    </>
+    </PopUpCallContext.Provider>
   );
 }
 
