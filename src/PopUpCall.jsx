bc66ef511d1d2e34b6f8d5d894a5a084a4586f1a
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-16 17:32:39 +0300

    Added PopUpCall component

---
+++ src/PopUpCall.jsx
@@ -0,0 +1,86 @@
+import React, { useContext, useState } from 'react';
+import { PopUpCallContext } from './App.js';
+
+
+const PopUpCall = () => {
+  const { popUpCallOpacity, togglePopUpCall } = useContext(PopUpCallContext);
+
+  const [ name, setName ] = useState('');
+  const [ phoneNumber, setPhoneNumber ] = useState('');
+  const [ checked, setChecked ] = useState(false);
+  const [ checkboxClass, setCheckboxClass ] = useState('inactive');
+
+  const handleNameChange = (event) => setName(event.target.value);
+  const handlePhoneNumberChange = (event) => setPhoneNumber(event.target.value);
+  
+  const toggleChecked = () => {
+    setCheckboxClass(checkboxClass === 'active' ? 'inactive' : 'active');
+    setChecked(!checked);
+  }
+
+  const handleFormChange = (event) => {
+    event.preventDefault();
+    alert(name + ' ' + phoneNumber);
+  }
+
+  return (
+    <div
+      className="pop-up-call"
+      style={{
+        opacity: popUpCallOpacity,
+        zIndex: popUpCallOpacity === 0 ? -1 : 9
+      }}
+    >
+      <div className="question">
+        Готовы обсудить проект?
+      </div>
+
+      <div className="description">
+        Заполните форму ниже и мы свяжемся с вами.
+      </div>
+
+      <form onSubmit={handleFormChange}>
+        <input
+          type="name"
+          placeholder="Имя*"
+          value={name}
+          onChange={handleNameChange}
+        />
+
+        <input
+          type="tel"
+          placeholder="Телефон*"
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
+        />
+
+        <div className="personalInfo">
+          <div
+            className={"fake-checkbox " + checkboxClass}
+            onClick={toggleChecked}
+          />
+          
+          <span className="description">
+            Я согласен с условиями обработки&nbsp;
+
+            <a href="/personalInfo">
+              персональных данных
+            </a>
+          </span>
+        </div>
+        
+
+        <input
+          className="send"
+          type="submit"
+          value="Отправить"
+        />      
+      </form>
+
+      <button className="close" onClick={togglePopUpCall} />
+    </div>
+  );
+};
+
+
+export default PopUpCall;
