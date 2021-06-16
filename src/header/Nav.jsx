7fa7dec04b63b320c9bc9991ecfacc1881133192
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-16 23:21:53 +0300

    Added path name of nav menu elements

--- src/header/Nav.jsx
+++ src/header/Nav.jsx
@@ -3,21 +3,25 @@ import React from 'react';
 
 const Nav = () => {
   const ul = [
-    'Главная',
-    'Проекты',
-    'Услуги',
-    'О компании',
-    'Готовые решения',
-    'Контакты'
+    {name: 'Главная', path: '/'},
+    {name: 'Проекты', path: '/projects'},
+    {name: 'Услуги', path: '/services'},
+    {name: 'О компании', path: '/about'},
+    {name: 'Готовые решения', path: '/solutions'},
+    {name: 'Контакты', path: '/contacts'}
   ];
 
   return (
     <nav>
       <ul>
         {ul.map((item, j) => (
-          <li key={"li_" + j}>
-            {item}
-          </li>
+          <a href={item.path} key={"a_" + j}>
+            {
+              (document.location.pathname === item.path)
+                ? <li className="active" key={"li_" + j}>{item.name}</li>
+                : <li key={"li_" + j}>{item.name}</li>
+            }
+          </a>
         ))}
       </ul>
     </nav>
