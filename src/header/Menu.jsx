0d8fb65590bd505a41e66df29bcd70ae4b427e4c
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-17 22:10:21 +0300

    Added open/close menu

--- src/header/Menu.jsx
+++ src/header/Menu.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './Nav.jsx';
 import Mail from './Mail.jsx';
 import Phone from './Phone.jsx';
 
 
 const Menu = () => {
+  const [ displayMenu, setDisplayMenu ] = useState('');
+  const [ togglerClass, setTogglerClass ] = useState('open');
+
+
+  const handleMenuToggler = () => {
+    setDisplayMenu(displayMenu === 'flex' ? 'none' : 'flex');
+    setTogglerClass(togglerClass === 'open' ? 'close' : 'open');
+  }
+
   return (
     <>
-      <button className="menu-toggler" />
+      <button
+        className={`menu-toggler ${togglerClass}`}
+        onClick={handleMenuToggler}
+      />
 
-      <div className="menu">
+      <div className="menu" style={{display: displayMenu}}>
 
         <Nav />
         
