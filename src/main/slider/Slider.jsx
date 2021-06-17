2bc85da23645288a82367b8e3b0bc00ea083f885
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-17 22:08:07 +0300

    Remade slider structure

--- src/main/slider/Slider.jsx
+++ src/main/slider/Slider.jsx
@@ -1,50 +1,16 @@
 import React, { useState } from 'react';
-import sliderImg_0_dt from './images/slider-img-0-dt.jpg';
-import sliderImg_1_dt from './images/slider-img-1-dt.jpg';
-import sliderImg_0_t from './images/slider-img-0-t.jpg';
-import sliderImg_1_t from './images/slider-img-1-t.jpg';
-import sliderImg_0_m from './images/slider-img-0-m.jpg';
-import sliderImg_1_m from './images/slider-img-1-m.jpg';
+import { slide_0, slide_1 } from './Slides.jsx';
 import DynamicContainer from './containers/dynamicContainer/DynamicContainer.jsx';
 import { StaticContainer, ArrowsContainer } from './containers/Containers.jsx';
 
 
-const SCREEN_WIDTH = window.screen.width;
-const SLIDER_IMAGES = [
-  [
-    sliderImg_0_dt,
-    sliderImg_1_dt,
-    sliderImg_0_dt,
-    sliderImg_1_dt,
-    sliderImg_0_dt
-  ],
-  [
-    sliderImg_0_t,
-    sliderImg_1_t,
-    sliderImg_0_t,
-    sliderImg_1_t,
-    sliderImg_0_t
-  ],
-  [
-    sliderImg_0_m,
-    sliderImg_1_m,
-    sliderImg_0_m,
-    sliderImg_1_m,
-    sliderImg_0_m
-  ]
+const slides = [
+  {num: 0, data: slide_0}, 
+  {num: 1, data: slide_1}, 
+  {num: 0, data: slide_0}, 
+  {num: 1, data: slide_1}, 
+  {num: 0, data: slide_0}
 ];
-
-let sliderImgs = [];
-
-if (SCREEN_WIDTH > 768) {
-  sliderImgs = SLIDER_IMAGES[0];
-
-} else if (SCREEN_WIDTH > 320) {
-  sliderImgs = SLIDER_IMAGES[1];
-
-} else {
-  sliderImgs = SLIDER_IMAGES[2];
-}
 
 
 const Slider = () => {
@@ -54,14 +20,11 @@ const Slider = () => {
   const handleNextSlide = () => setCurrentSlide(currentSlide + 1);
   
   return (
-    <section
-      className="slider"
-      style={{
-        background: `white url(${sliderImgs[currentSlide]})`
-      }}
-    >
+    <section className="slider" >
+      <div className={`slider-background-img img-${slides[currentSlide].num}`}></div>
       <StaticContainer />
       <DynamicContainer
+        slides={slides}
         currentSlide={currentSlide}
         setCurrentSlide={setCurrentSlide}
       />
--- src/main/slider/containers/dynamicContainer/DynamicContainer.jsx
+++ src/main/slider/containers/dynamicContainer/DynamicContainer.jsx
@@ -1,20 +1,11 @@
 import React, { useEffect } from 'react';
-import { slide_0, slide_1 } from './Slides.jsx';
 
-
-const slides = [
-  slide_0,
-  slide_1,
-  slide_0,
-  slide_1,
-  slide_0
-];
 
 const slidesTransition = {value: '0.3s'};
 
 
 const DynamicContainer = (props) => {
-  const { currentSlide, setCurrentSlide } = props;
+  const { slides, currentSlide, setCurrentSlide } = props;
 
   useEffect(() => {
     setTimeout(() => {
@@ -44,11 +35,11 @@ const DynamicContainer = (props) => {
           (j === currentSlide)
             ? (
               <div className="slide active" key={`slide-${j}`}>
-                {item}
+                {item.data}
               </div>
             ) : (
               <div className="slide inactive" key={`slide-${j}`}>
-                {item}
+                {item.data}
               </div>
             )
         ))}
