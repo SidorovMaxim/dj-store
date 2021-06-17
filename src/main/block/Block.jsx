5e09ea60f5b8cbb45a582c7e8b76b2f5d04b4850
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-17 22:05:28 +0300

    Remade section 'block' background images structure

--- src/main/block/Block.jsx
+++ src/main/block/Block.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import blockImg1 from '../images/block_image_1.jpg';
-import blockImg2 from '../images/block_image_2.jpg';
 import ToolTipMixer from './ToolTipMixer.jsx';
+
 
 const Block = () => {
   return (
     <section className="block">
-      <div className="img-container">
-        <img src={blockImg1} alt="block-img" />
-      </div>
-
-      <div className="img-container">
-        <img src={blockImg2} alt="block-img" />
-      </div>
-
+      <div className="img-container img-0" />
+      <div className="img-container img-1" />
       <ToolTipMixer />
     </section>
   );
