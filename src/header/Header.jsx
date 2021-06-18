3fc06a23ef0183617e2553d97045bd4c8df677cf
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-18 22:58:22 +0300

    Added sticky header onScrollTop

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,12 +1,42 @@
 import './Header.scss';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './svg/logo.svg';
 import Menu from './Menu.jsx';
 
 
+const scroll = {
+  prev: 0,
+  current: 0
+}
+
+
 const Header = () => {
+  const [ headerPosition, setHeaderPosition ] = useState(0);
+
+  const handleScroll = () => {
+    scroll.current = window.pageYOffset;
+    
+    if (scroll.current < scroll.prev) {
+      setHeaderPosition(0);
+      
+    } else {
+      setHeaderPosition(-100);
+    }
+
+    scroll.prev = scroll.current;
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll);
+    return () => document.removeEventListener('scroll', handleScroll);
+  });
+
   return (
-    <header>
+    <header
+      style={{
+        transform: `translateY(${headerPosition}%)`
+      }}
+    >
       <a className="logo" href="/">
         <img src={logo} alt="logo" />
       </a>
