08850f1a81ee4a0ba0e1cd5199fe128e00ad13b4
SidorovMaxim <maximsidorov1996@gmail.com>
2021-06-18 23:01:22 +0300

    Added ajax request and result elements of pop-up-call form

--- src/popUpCall/PopUpCall.jsx
+++ src/popUpCall/PopUpCall.jsx
@@ -4,13 +4,18 @@ import { PopUpCallContext } from '../App.js';
 
 
 const PopUpCall = () => {
+  
+  // Context
   const { popUpCallOpacity, togglePopUpCall } = useContext(PopUpCallContext);
 
+  // State
+  const [ status, setStatus ] = useState('showForm');
   const [ name, setName ] = useState('');
   const [ phoneNumber, setPhoneNumber ] = useState('');
   const [ checked, setChecked ] = useState(false);
   const [ checkboxClass, setCheckboxClass ] = useState('inactive');
 
+  // Handlers
   const handleNameChange = (event) => setName(event.target.value);
   const handlePhoneNumberChange = (event) => setPhoneNumber(event.target.value);
   
@@ -19,68 +24,136 @@ const PopUpCall = () => {
     setChecked(!checked);
   }
 
-  const handleFormChange = (event) => {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    alert(name + ' ' + phoneNumber);
+
+    if (checked) {
+      const user = {name, phoneNumber};
+
+      fetch('/post/user', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify(user)
+        })
+        .then(
+          (result) => {
+            setStatus('success');
+          },
+          (error) => {
+            setStatus('error');
+          }
+        );
+
+    } else {
+      setCheckboxClass('unchecked');
+    }
   }
 
-  return (
-    <div
-      className="pop-up-call"
-      style={{
-        opacity: popUpCallOpacity,
-        zIndex: popUpCallOpacity === 0 ? -1 : 9
-      }}
-    >
-      <div className="question">
-        Готовы обсудить проект?
-      </div>
-
-      <div className="description">
-        Заполните форму ниже и мы свяжемся&nbsp;с&nbsp;вами.
-      </div>
-
-      <form onSubmit={handleFormChange}>
-        <input
-          type="name"
-          placeholder="Имя*"
-          value={name}
-          onChange={handleNameChange}
-        />
-
-        <input
-          type="tel"
-          placeholder="Телефон*"
-          value={phoneNumber}
-          onChange={handlePhoneNumberChange}
-        />
-
-        <div className="personalInfo">
-          <div
-            className={"fake-checkbox " + checkboxClass}
-            onClick={toggleChecked}
-          />
-          
-          <span className="description">
-            Я согласен с условиями обработки{" "}
-
-            <a href="/personalInfo">
-              персональных&nbsp;данных
-            </a>
-          </span>
+  // Render
+  if (status === 'showForm') {
+    return (
+      <div
+        className="pop-up-call"
+        style={{
+          opacity: popUpCallOpacity,
+          zIndex: popUpCallOpacity === 0 ? -1 : 9
+        }}
+      >
+        <div className="question">
+          Готовы обсудить проект?
         </div>
-        
 
-        <input
-          className="send"
-          type="submit"
-          value="Отправить"
-        />      
-      </form>
+        <div className="description">
+          Заполните форму ниже и мы свяжемся&nbsp;с&nbsp;вами.
+        </div>
 
-      <button className="close" onClick={togglePopUpCall} />
-    </div>
-  );
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="name"
+            placeholder="Имя*"
+            value={name}
+            onChange={handleNameChange}
+          />
+
+          <input
+            type="tel"
+            placeholder="Телефон*"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+          />
+
+          <div className="personalInfo">
+            <div
+              className={"fake-checkbox " + checkboxClass}
+              onClick={toggleChecked}
+            />
+            
+            <span className="description">
+              Я согласен с условиями обработки{" "}
+
+              <a href="/personalInfo">
+                персональных&nbsp;данных
+              </a>
+            </span>
+          </div>
+          
+
+          <input
+            className="send"
+            type="submit"
+            value="Отправить"
+          />      
+        </form>
+
+        <button className="close" onClick={togglePopUpCall} />
+      </div>
+    );    
+
+  } else if (status === 'succes') {
+    return (
+      <div
+        className="pop-up-call"
+        style={{
+          opacity: popUpCallOpacity,
+          zIndex: popUpCallOpacity === 0 ? -1 : 9
+        }}
+      >
+        <div className="result">
+          Заявка принята
+        </div>
+
+        <button className="confirm" onClick={togglePopUpCall}>
+          Ок
+        </button>
+
+        <button className="close" onClick={togglePopUpCall} />
+      </div>     
+    );
+
+  } else { 
+    return (
+      <div
+        className="pop-up-call"
+        style={{
+          opacity: popUpCallOpacity,
+          zIndex: popUpCallOpacity === 0 ? -1 : 9
+        }}
+      >
+        <div className="result">
+          Нет ответа от сервера
+        </div>
+
+        <button className="confirm" onClick={togglePopUpCall}>
+          Ок
+        </button>
+
+        <button className="close" onClick={togglePopUpCall} />
+      </div>      
+    );
+  }
+
 };
 
 
